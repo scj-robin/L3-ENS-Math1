8390985c8aa65d4b3068b2e697b6b516db3989eb
Stéphane Robin <stephane.robin@sorbonne-universite.fr>
2022-12-07 14:11:21 +0100

    TD

--- Codes/FigTD-SystDyn.R
+++ Codes/FigTD-SystDyn.R
@@ -1,9 +1,10 @@
-# Figures du chapitre EDO / sytèmes dynamiques
+# Figures du chapitre EDO2 / sytèmes dynamiques
 
 rm(list=ls()); par(pch=20)
 figDir <- '../Figures/'
 exportFig <- TRUE
 par(pch=20, lwd=1); lwd=4
+source('FonctionsSystDyn.R')
 
 ################################################################################
 # Exo L2 bio SU
@@ -20,50 +21,50 @@ if(exportFig){dev.off()}
 ################################################################################
 # Exo activation réciproque / compétition intra-spécifique
 exName <- 'ActivationReciproque'
-library(plotrix); library(lattice); library(deSolve)
-a <- 1/2
-EDO <- function(y, a=1){c(a*y[2] - y[1]^2, a*y[1] - y[2]^2)}
+parm <- list(a=1/2)
+EDO2 <- function(y, parm){c(parm$a*y[2] - y[1]^2, parm$a*y[1] - y[2]^2)}
+# Grid for gradient field
+xGrid <- yGrid <- seq(0, 2*parm$a, length.out=20); nGrid <- length(xGrid)
+xyGrid <- rbind(as.matrix(expand.grid(xGrid, yGrid)), cbind(xGrid, xGrid^2/parm$a), cbind(xGrid, sqrt(parm$a*xGrid)))
 
-# Gradient field
-xGrid <- yGrid <- seq(0, 2*a, length.out=20); nGrid <- length(xGrid)
-xyGrid <- as.matrix(expand.grid(xGrid, yGrid))
-xyGrid <- rbind(xyGrid, cbind(xGrid, xGrid^2/a), cbind(xGrid, sqrt(a*xGrid)))
-grad <- t(sapply(1:nrow(xyGrid), function(i){EDO(xyGrid[i, ], a=a)}))
-
-# Solutions
-parms <- c(a=a); 
-model <- function(t, y, parms){return(list(EDO(y, a=parms[1])))}
-
-y0List <- list(c(a/2, a/2), c(a/2, 3*a/2), c(3*a/2, a/2), c(3*a/2, 3*a/2))
-solList <- list()
-for(i in 1:length(y0List)){
-  solList[[i]] <- ode.2D(y0List[[i]], times=seq(0, 10, by=.001), func=model, 
-                         nspec=2, parms=parms, dimens=c(1, 1), method='ode45')
-}
+# Init
+y0List <- list(c(parm$a/2, parm$a/2), c(parm$a/2, 3*parm$a/2), 
+               c(3*parm$a/2, parm$a/2), c(3*parm$a/2, 3*parm$a/2))
+times <- seq(0, 10, by=.001)
 
 # Figure
 if(exportFig){png(paste0(figDir, exName, '.png'))}
-plot(xGrid, yGrid, col=0, xlab='x', ylab='y')
-abline(h = 0, v = 0, col=1)
-vectorField(grad[, 1], grad[, 2], as.vector(xyGrid[, 1]), as.vector(xyGrid[, 2]), col=8)
-abline(h = a, v = a, col=1, lwd=2)
-curve(sqrt(a*x), from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
-curve(x^2/a, from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
-for(i in 1:length(y0List)){
-  points(y0List[[i]][1], y0List[[i]][2], col=1+i, pch=20)
-  lines(solList[[i]][, 2], solList[[i]][, 3], col=1+i, lwd=2)
-}
+PlotSystDyn2D(edo=EDO2, y0List=y0List, xGrid=xGrid, yGrid=yGrid, xyGrid=xyGrid, 
+              parm=parm, times=times, href=parm$a, vref=parm$a)
+curve(sqrt(parm$a*x), from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
+curve(x^2/parm$a, from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
 if(exportFig){dev.off()}
 
 if(exportFig){png(paste0(figDir, exName, '-zoom.png'))}
-plot(xGrid, yGrid, col=0, xlab='x', ylab='y', xlim=c(0.9*a, 1.1*a), ylim=c(0.9*a, 1.1*a))
-abline(h = 0, v = 0, col=1)
-vectorField(grad[, 1], grad[, 2], as.vector(xyGrid[, 1]), as.vector(xyGrid[, 2]), col=8)
-abline(h = a, v = a, col=1, lwd=2)
-curve(sqrt(a*x), from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
-curve(x^2/a, from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
-for(i in 1:length(y0List)){
-  points(y0List[[i]][1], y0List[[i]][2], col=1+i, pch=20)
-  lines(solList[[i]][, 2], solList[[i]][, 3], col=1+i, lwd=2)
-}
+PlotSystDyn2D(edo=EDO2, y0List=y0List, xGrid=xGrid, yGrid=yGrid, xyGrid=xyGrid, 
+              xlim=parm$a*c(0.9, 1.1), ylim=parm$a*c(0.9, 1.1),
+              parm=parm, times=times, href=parm$a, vref=parm$a)
+curve(sqrt(parm$a*x), from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
+curve(x^2/parm$a, from=min(xGrid), to=max(xGrid), add=TRUE, col=8)
+if(exportFig){dev.off()}
+
+################################################################################
+# Dyn 3 pop (mâles, femelles, couples)
+exName <- 'DynPopMaleFemelleCouple'
+parm <- list(a=1, c=1, r=1, S=.5)
+EDO3 <- function(y, parm){c(-parm$a*(y[1]^2 + parm$S*y[1]) + parm$r*y[2],  
+                                          parm$a*(y[1]^2 + parm$S*y[1]) - parm$c*y[2]^2)}
+Delta <- parm$S^2 + 4*parm$r^2/parm$a/parm$c; 
+zStar <- parm$r/parm$c; yStar <- (sqrt(Delta) - parm$S)/2
+# Grid
+yGrid <- zGrid <- seq(0, 2*max(yStar, zStar), length.out=25); nGrid <- length(yGrid)
+# Init
+y0List <- list(c(0, 0), c(yStar/2, zStar/2), c(yStar/2, 3*zStar/2), 
+               c(3*yStar/2, zStar/2), c(3*yStar/2, 3*zStar/2))
+# Figure
+if(exportFig){png(paste0(figDir, exName, '.png'))}
+PlotSystDyn2D(edo=EDO3, y0List=y0List, xGrid=yGrid, yGrid=zGrid, 
+              parm=parm, href=zStar, vref=yStar, xlab='y', ylab='z')
+curve(sqrt(parm$a/parm$c*(x^2 + parm$S*x)), from=min(yGrid), to=max(yGrid), n=1001, add=TRUE, col=8)
+curve(parm$a/parm$r*(x^2 + parm$S*x), from=min(yGrid), to=max(yGrid), n=1001, add=TRUE, col=8)
 if(exportFig){dev.off()}
