a4eed7471a61e74350d0723585a6a29d0c3add14
Stéphane Robin <stephane.robin@sorbonne-universite.fr>
2022-12-13 09:31:10 +0100

    BGW

--- Codes/FigGaltonWatson.R
+++ Codes/FigGaltonWatson.R
@@ -27,14 +27,15 @@ sStar <- sGrid[min(which(abs(fGrid-sGrid) < 1e-3))]
 
 # Iteration de la fonction génératrice
 Z0 <- 1; nMax <- 100
-q0 <- 0
+q0 <- .00
 qVec <- rep(0, 1+nMax); qVec[1] <- q0
 if(exportFig){png(paste0(figDir, dataName, '-fixPoint-q0', round(100*q0), '.png'))}
 for(n in 1:nMax){qVec[n+1] <- fX(qVec[n])}
-curve(fX, ylim=c(0, 1), col=2, lwd=2, xlab='s', ylab='fX(s)')
+curve(fX, ylim=c(0, 1), col=4, lwd=2, xlab='s', ylab='fX(s)')
 abline(0, 1, col=8, lwd=2)
+abline(v=qVec[nMax], col=4, lty=2, lwd=2)
 abline(h=c(0, 1), v=c(0, 1))
-lines(c(q0, qVec[-(nMax+1)]), c(0, qVec[-1]), type='s', col=4, lwd=2)
+lines(c(q0, qVec[-(nMax+1)]), c(0, qVec[-1]), type='s', col=2, lwd=2)
 if(exportFig){dev.off()}
 
 # # Cas géométrique
