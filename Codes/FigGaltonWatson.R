ad6ab0e236544ccc37284de83420a3201e22fbf5
Stéphane Robin <stephane.robin@sorbonne-universite.fr>
2023-10-11 13:37:17 +0200

    GW

--- Codes/FigGaltonWatson.R
+++ Codes/FigGaltonWatson.R
@@ -11,13 +11,13 @@ fX <- function(s){exp(lambda*(s-1))}
 lambdaList <- c(.5, 1, 1.5)
 if(exportFig){png(paste0(figDir, dataName, '-pgf.png'))}
 lambda <- lambdaList[1]
-curve(fX, ylim=c(0, 1), col=2, lwd=2, xlab='s', ylab='fX(s)')
+curve(fX, ylim=c(0, 1), col=2, lwd=3, xlab='s', ylab='fX(s)')
 for(i in 2:length(lambdaList)){
    lambda <- lambdaList[i]
-   curve(fX, ylim=c(0, 1), col=1+i, lwd=2, xlab='s', ylab='fX(s)', add=TRUE)
+   curve(fX, ylim=c(0, 1), col=1+i, lwd=3, xlab='s', ylab='fX(s)', add=TRUE)
 }
-abline(0, 1, col=1, lwd=2)
-abline(h=c(0, 1), v=c(0, 1), col=8, lwd=2)
+abline(0, 1, col=1, lwd=3)
+abline(h=c(0, 1), v=c(0, 1), col=8)
 if(exportFig){dev.off()}
 
 # Point fixe
@@ -28,15 +28,15 @@ sStar <- sGrid[min(which(abs(fGrid-sGrid) < 1e-3))]
 
 # Iteration de la fonction génératrice
 Z0 <- 1; nMax <- 100
-q0 <- .00
+q0 <- .75
 qVec <- rep(0, 1+nMax); qVec[1] <- q0
 if(exportFig){png(paste0(figDir, dataName, '-fixPoint-q0', round(100*q0), '.png'))}
 for(n in 1:nMax){qVec[n+1] <- fX(qVec[n])}
-curve(fX, ylim=c(0, 1), col=4, lwd=2, xlab='s', ylab='fX(s)')
-abline(0, 1, col=1, lwd=2)
-abline(v=qVec[nMax], col=4, lty=2, lwd=2)
+curve(fX, ylim=c(0, 1), col=4, lwd=3, xlab='s', ylab='fX(s)')
+abline(0, 1, col=1, lwd=3)
+abline(v=qVec[nMax], col=4, lty=2, lwd=3)
 abline(h=c(0, 1), v=c(0, 1), col=8)
-lines(c(q0, qVec[-(nMax+1)]), c(0, qVec[-1]), type='s', col=2, lwd=2)
+lines(c(q0, qVec[-(nMax+1)]), c(0, qVec[-1]), type='s', col=2, lwd=3)
 if(exportFig){dev.off()}
 
 # # Cas géométrique
@@ -45,3 +45,13 @@ if(exportFig){dev.off()}
 # curve(fX, ylim=c(0, 1), col=2, lwd=2, xlab='s', ylab='fX(s)')
 # abline(0, 1, col=1, lwd=2)
 # abline(v = (1-a)/a, lwd=2, col=4)
+# abline(h=c(0, 1), v=c(0, 1), col=8)
+
+# Cas Poisson
+fX <- function(s){exp(lambda*(s-1))}
+gX <- function(s){fX(s)-s}
+lambda <- 4/3
+curve(fX, ylim=c(0, 1), col=2, lwd=2, xlab='s', ylab='fX(s)')
+abline(0, 1, col=1, lwd=2)
+abline(v = uniroot(gX, c(1e-4, 1-1e-4))$root, lwd=2, col=4)
+abline(h=c(0, 1), v=c(0, 1), col=8)
