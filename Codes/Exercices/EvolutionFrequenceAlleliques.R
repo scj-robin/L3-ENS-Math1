11d4cd5363286b4878f051bfd12489de3750b980
Stéphane Robin <stephane.robin@sorbonne-universite.fr>
2024-12-12 21:21:34 +0100

    code exam

--- Codes/Exercices/EvolutionFrequenceAlleliques.R
+++ Codes/Exercices/EvolutionFrequenceAlleliques.R
@@ -1,3 +1,5 @@
+rm(list=ls()); palette('R3')
+
 m <- 5
 
 DrawA <- function(p){A <- rgamma(m, m, 1)*matrix(runif(m^2), m, m); A <- A + t(A); A / max(A)}
@@ -6,25 +8,28 @@ Viabilite <- function(x){x <- x/sum(x); (t(x)%*%A%*%x)[1, 1]}
 
 A <- DrawA(p)
 while(sum(eigen(A)$values > 0)!=1){A <- DrawA(p)}
-
-tMax <- 1000
-pMat <- matrix(NA, tMax, m)
-pMat[1, ] <- rep(1/m, m)
-for(t in 2:tMax){
-  pMat[t, ] <- pMat[t-1, ] * (A %*% pMat[t-1, ])
-  pMat[t, ] <- pMat[t, ] / sum(pMat[t, ])
-}
-matplot(pMat, ylim=c(0, 1))
 V <- eigen(A)$vectors
 lambda <- eigen(A)$values
 v1 <- V[, 1] / sum(V[, 1])
+
+tMax <- 50
+pPath <- matrix(NA, tMax, m)
+pPath[1, ] <- rep(1/m, m)
+for(t in 2:tMax){
+  pPath[t, ] <- pPath[t-1, ] * (A %*% pPath[t-1, ])
+  pPath[t, ] <- pPath[t, ] / sum(pPath[t, ])
+}
+pMax <- pPath[tMax, ]
+VPath <- sapply(1:tMax, function(t){Viabilite(pPath[t, ])})
+
+par(mfrow=c(2, 1))
+matplot(pPath, ylim=c(0, 1), type='l', lwd=2, lty=1)
 abline(h=v1, col=1:m, lty=2)
+plot(VPath, type='l', lwd=2)
+abline(h=Viabilite(v1), lty=2)
 
-Viabilite <- function(x){x <- x/sum(x); (t(x)%*%A%*%x)[1, 1]}
-
-opt <- optim(par=rep(1/m, m), fn=Viabilite, control=list(fnscale=-1))$par
-opt <- opt/sum(opt)
-
-Viabilite(opt)
-Viabilite(pMat[tMax, ])
+Viabilite(pMax)
+sum(lambda*((t(V)%*%pMax)^2))
 Viabilite(v1)
+lambda[1]*sum(v1^2)
+t(V)%*%pMax
